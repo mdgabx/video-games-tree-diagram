ea584f1164d803f7c9c77252ebb40edd279204fb
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-01-24 19:14:06 +0800

    modify the structure

--- src/Components/Tree.jsx
+++ src/Components/Tree.jsx
@@ -1,11 +1,12 @@
-import Chart from "./Chart";
+import * as d3 from 'd3';
 
 const Tree = () => {
     return ( 
         <div className="container">
             <h1 id="title">Video Games Tree Diagram</h1>
             <p id="description">Top 100 Most Sold Video Games Grouped by Platform</p>
-            <Chart />
+            <div className="chart-con">
+            </div>
         </div>
      );
 }
