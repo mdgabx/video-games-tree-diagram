a2c789b03f7f42267180359c8f52f8c293a6fc36
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-01-25 16:36:48 +0800

    adding the data on the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import Tree from "./Components/Tree";
 
 function App() {
+  const gamesUrl = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+  const [gamesData, setGamesData]  = useState("");
+
+  useEffect(() => {
+    fetch(gamesUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setGamesData(data);
+      })
+  }, [setGamesData])
+
   return (
     <div className="App">
-      <Tree />
+      <Tree data={gamesData}/>
     </div>
   );
 }
--- src/Components/Tree.jsx
+++ src/Components/Tree.jsx
@@ -1,9 +1,32 @@
 import * as d3 from 'd3';
 
-const Tree = () => {
+const Tree = ({data}) => {
+
+    const width = 1000;
+    const height = 800;
+
+    if(data !== "" && data !== undefined && data !== null) {
+
+       const svg = d3.select(".chart-con")
+                        .append("svg")
+                        .attr("width", width)
+                        .attr("height", height)
+
+        svg.selectAll("rect")
+                .data(data.children)
+                .enter()
+                .append("rect")
+                .attr("class", "tile")
+
+        
+    }
+
+
+
+
     return ( 
         <div className="container">
-            <h1 id="title">Video Games Tree Diagram</h1>
+            <h1 id="title">{data.name}</h1>
             <p id="description">Top 100 Most Sold Video Games Grouped by Platform</p>
             <div className="chart-con">
             </div>
