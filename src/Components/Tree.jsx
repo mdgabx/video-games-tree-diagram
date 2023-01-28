aea77aa0389602db302015154f0cbcf4e9184c08
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-01-28 23:10:26 +0800

    add the legends

--- src/Components/Tree.jsx
+++ src/Components/Tree.jsx
@@ -17,7 +17,9 @@ const Tree = ({data}) => {
         //             console.log('data', d.name);
         //         })
 
-        let canvas = d3.select("#canvas");
+        let canvas = d3.select("#canvas")
+                        .attr("width", width)
+                        .attr("height", height)
 
 
         //setting the hierarchy for the tree map
@@ -41,6 +43,9 @@ const Tree = ({data}) => {
                 .data(vGameTiles)
                 .enter()
                 .append("g")
+                .attr("transform", (game) => {
+                    return `translate(${game["x0"]}, ${game["y0"]})`
+                })
 
         block.append("rect")
             .attr("class", "tile")
@@ -75,22 +80,43 @@ const Tree = ({data}) => {
                 } else if (category === "N64") {
                     return "skyblue"
                 } else if (category === "GBA") {
-                    return "emerald"
+                    return "cyan"
                 } else if (category === "XB") {
                     return "gold"
                 } else if (category === "PC") {
-                    return "black"
+                    return "tan"
                 } else if (category === "2600" ) {
                     return "brown"
                 } else if (category === "PSP") {
                     return "pink"
                 } else {
-                    return "scarlet";
+                    return "navy";
                 }
             })
-    }
+            .attr('data-name', (game) => {
+                return game["data"]["name"];
+            })
+            .attr('data-category', (game) => {
+                return game["data"]["category"]
+            })
+            .attr('data-value', (game) => {
+                return game["data"]["value"]
+            })
+            .attr("width", (game) => {
+                return  game["x1"] - game["x0"];
+            })
+            .attr("height", (game) => {
+                return game["y1"] - game["y0"];
+            })
 
+            block.append("text")
+                .text((game) => game["data"]["name"])
+                .attr('x', 5)
+                .attr('y', 20)
+            }
 
+        const tileWidth = 40;
+        const tileHeight = 30;
 
 
     return ( 
@@ -100,6 +126,80 @@ const Tree = ({data}) => {
             <div className="chart-con">
                 <svg id="canvas">
 
+                </svg>
+                <svg id="legend">
+                    <g>
+                        <rect className="legend-item" x="10" y="40" width={tileWidth} height={tileHeight} fill="blue"></rect>
+                        <text x="60" y="60">Wii</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">DS</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="100" y="40" width={tileWidth} height={tileHeight} fill="lightblue"></rect>
+                        <text x="150" y="60">X360</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="100" y="90" width={tileWidth} height={tileHeight} fill="green"></rect>
+                        <text x="150" y="110">GB</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="190" y="40" width={tileWidth} height={tileHeight} fill="yellow"></rect>
+                        <text x="240" y="60">PS3</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="190" y="90" width={tileWidth} height={tileHeight} fill="maroon"></rect>
+                        <text x="240" y="110">NES</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="280" y="40" width={tileWidth} height={tileHeight} fill="lightgreen"></rect>
+                        <text x="330" y="60">PS2</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="280" y="90" width={tileWidth} height={tileHeight} fill="gray"></rect>
+                        <text x="330" y="110">3DS</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">PS4</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">SNES</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">PS</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">N64</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">GBA</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">XB</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">PC</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">2600</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">PSP</text>
+                    </g>
+                    <g>
+                        <rect className="legend-item" x="10" y="90" width={tileWidth} height={tileHeight} fill="orange"></rect>
+                        <text x="60" y="110">XOne</text>
+                    </g>
                 </svg>
             </div>
         </div>
